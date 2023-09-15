5bd39e1d0e1ef1d3b59d75f076e208b7cfc577b3
Kateryna <k.churuta@gmail.com>
2023-09-15 16:18:47 +0100

    color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let myIdInterval;
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function startColorChange() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    myIdInterval = setInterval(() => {
+    let color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+}
+
+function stopColorChange() {
+        clearInterval(myIdInterval);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', startColorChange);
+stopBtn.addEventListener('click',stopColorChange)
+
