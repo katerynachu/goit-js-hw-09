25c114c363f731b0a19f1f1613a944e7765a85d7
katerynachu <83462529+katerynachu@users.noreply.github.com>
2023-09-16 08:51:53 +0000

    Deploying to gh-pages from @ katerynachu/goit-js-hw-09@2998cb62615f74373a924ab0602bb47897892157 🚀

---
+++ 01-color-switcher.69817f1f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),e.disabled=!0,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.69817f1f.js.map
